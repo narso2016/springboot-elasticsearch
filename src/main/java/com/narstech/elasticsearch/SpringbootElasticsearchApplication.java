306814a6b2bc8f5ff7a8b39e3d36d9b27ca1b041
Munarso <narsomunarso@gmail.com>
2023-04-26 14:00:13 +0700

    first commit

---
+++ src/main/java/com/narstech/elasticsearch/SpringbootElasticsearchApplication.java
@@ -0,0 +1,14 @@
+package com.narstech.springbootelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootElasticsearchApplication.class, args);
+		System.out.println("Elastic Search Application Started!!!!");
+	}
+
+}
